4df35c324b9f2acd6c6aaf5a1b7a5dcc12f76879
Jacob <jguad411@icloud.com>
2026-07-21 19:55:19 -0500

    made robot drive and use auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousOpMode.java
@@ -2,18 +2,55 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 @Autonomous(name="Autonomous")
 public class AutonomousOpMode extends LinearOpMode {
+    private DcMotor leftMotor;
+    private DcMotor rightMotor;
+
+
+
+
+    private ElapsedTime mytime = new ElapsedTime();
+
 
     /* Declare OpMode members such as motor variables */
 
     @Override
     public void runOpMode() {
         // Initialize Motors
+        leftMotor = hardwareMap.get(DcMotor.class, "leftBack");
+        rightMotor = hardwareMap.get(DcMotor.class, "rightBack");
+// sets motors to their name in control hub
+        rightMotor.setDirection(DcMotor.Direction.REVERSE);
+        leftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+//sets direction and zero power behavior
 
         // Wait for the game to start (driver presses START)
         waitForStart();
+        drive(1,0,.5);
+        drive(0,1,5);
+
+
+
+    }
+    private void drive(double fowardSpeed, double turnSpeed ,double timer) {
+       mytime.reset();
+        leftMotor.setPower(fowardSpeed - turnSpeed);
+        rightMotor.setPower(fowardSpeed + turnSpeed);
+
+      while(opModeIsActive()&& mytime.seconds()<timer){
+          telemetry.addLine("1 1");
+          telemetry.addLine(" - ");
+          telemetry.update();
+      }
+
+
+            leftMotor.setPower(0);
+            rightMotor.setPower(0);
 
 
     }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleopOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleopOpMode.java
@@ -2,21 +2,60 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
 @TeleOp(name="Teleop")
 public class TeleopOpMode extends LinearOpMode {
+    private DcMotor leftMotor;
+    private DcMotor rightMotor;
 
     /* Declare OpMode members such as motor variables */
 
     @Override
     public void runOpMode() {
+
+
+        leftMotor = hardwareMap.get(DcMotor.class, "leftBack");
+        rightMotor = hardwareMap.get(DcMotor.class, "rightBack");
+        //sets motors to their name
+        rightMotor.setDirection(DcMotor.Direction.REVERSE);
+        leftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        //sets motor direction and brake behavior
+
+
         // Initialize Motors
+double fowardSpeed =0;
+double turnSpeed =0;
+double leftPower=0;
+double rightPower=0;
+double drivemultiplier = 1;
+
 
         // Wait for the game to start (driver presses START)
         waitForStart();
 
         // run until the end of the match (driver presses STOP)
         while (opModeIsActive()) {
+
+            fowardSpeed = -gamepad1.left_stick_y;
+            turnSpeed = -gamepad1.right_stick_x;
+            if(gamepad1.left_bumper){
+                drivemultiplier = 0.32;
+            }
+            else{
+                drivemultiplier = 1;
+            }
+
+            //sets varible to amount gamepad moved
+
+            rightPower = (fowardSpeed + turnSpeed) * drivemultiplier;
+            leftPower=(fowardSpeed - turnSpeed)*drivemultiplier;
+            //sets power of motor power varible
+
+            leftMotor.setPower(leftPower);
+            rightMotor.setPower(rightPower);
+            //sets power of motor to power varible
 
         }
     }
